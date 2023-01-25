4d2ad899468684723e318be15c22eb7f660adf3d
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-01-26 02:35:14 +0900

    style: 로그인 폼 스타일 수정

--- src/components/Signin/style.js
+++ src/components/Signin/style.js
@@ -1,69 +1,20 @@
 import styled from '@emotion/styled';
-import '../../index.css';
-
-const LoginTitle = styled.div`
-  font-size: var(--font-title-size);
-`;
 
 const LoginBlock = styled.div`
   width: 100%;
+  height: calc(100vh - 80px);
   display: flex;
-  height: 100vh;
   justify-content: center;
   align-items: center;
   background-color: var(--background-color);
 `;
 
 const LoginWrapper = styled.div`
-  width: 400px;
-  height: 10%;
-  padding: 5rem;
-  border-radius: 2px;
-  position: relative;
-  bottom: 30%;
-`;
-
-const Fieldset = styled.fieldset`
-  height: 2.5rem;
-  border-radius: 4px;
-  margin-bottom: 20px;
-  border: 1px solid;
-`;
-
-const LoginContainer = styled.div`
-  margin-top: 1rem;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  border-radius: 2px;
-  outline: none;
-  font-size: var(--font-input-size);
-  background-color: var(--background-color);
-
-  &:focus {
-    color: #495057;
-    border-color: black;
-  }
-`;
-
-const FormSignupText = styled.p`
-  text-align: center;
-  font-size: var(--font-subtitle-size);
-`;
-
-const FormLoginText = styled.p`
-  font-size: var(--font-subtitle-size);
-  margin-right: 50px;
-  color: var(--font-input-color);
-`;
-
-const FormFailedText = styled.p`
-  font-size: 10px;
-  text-align: center;
+  width: 430px;
 `;
 
 const FormLogin = styled.form`
+  padding: 0 30px;
   h3 {
     font-size: 1.5rem;
     margin-right: 200px;
@@ -76,14 +27,59 @@ const FormLogin = styled.form`
   }
 `;
 
+const LoginTitle = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+const FormLoginText = styled.p`
+  font-size: 14px;
+  margin-bottom: 20px;
+  color: var(--font-input-color);
+`;
+
+const Fieldset = styled.fieldset`
+  margin-top: 12px;
+  border-radius: 4px;
+  border: 1px solid;
+  overflow: hidden;
+  border: 2px solid #dee2e6;
+  font-size: 13px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  outline: none;
+  font-size: 14px;
+  background-color: var(--background-color);
+`;
+
+const FormSignupText = styled.p`
+  text-align: center;
+  font-size: 14px;
+
+  span {
+    display: inline-block;
+    margin-left: 10px;
+    font-weight: 700;
+  }
+`;
+
+const FormFailedText = styled.p`
+  text-align: center;
+  font-size: 13px;
+  margin-left: 5px;
+  color: #e63946;
+`;
+
 const FormButton = styled.button`
   width: 100%;
-  height: 3rem;
-  padding: 0.7rem;
+  padding: 0.8rem;
   border: 1px solid #dee2e6;
   border-radius: 5px;
   outline: none;
-  font-size: 1.2rem;
+  font-size: 1rem;
+  font-weight: 700;
   margin-top: 1rem;
   text-align: center;
   background-color: #8dd3bb;
@@ -94,13 +90,16 @@ const FormButton = styled.button`
     border-color: #80bdff;
     box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
   }
+
+  &:hover {
+    background-color: #55d4a9;
+  }
 `;
 
 export {
   LoginTitle,
   LoginBlock,
   LoginWrapper,
-  LoginContainer,
   Input,
   FormSignupText,
   FormLoginText,
